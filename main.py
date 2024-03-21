4cebf0ad631ab2560f57343fd28803ae894fe269
Edward Mike <greenpython.developer@gmail.com>
2024-03-21 21:49:08 +0000

    script to scrap web data

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-def main() -> None:
-    print("Hello FaQ Bot")
+from scrap import extract_contents_for
+
+ALLOW_SCRAPPING = True # Toggle to enable web scrapping. 
+
+
+def main():
+    urls = [
+        "https://www.stir.ac.uk/study/fees-funding/",
+        "https://www.stir.ac.uk/study/semester-dates/",
+        "https://www.stir.ac.uk/about/contact-us/",
+        "https://www.stir.ac.uk/study/fees-funding/how-to-pay-your-fees/",
+        "https://www.stir.ac.uk/internal-students/fees-and-finance/online-payment-terms-and-conditions/",
+    ]
     
-    
+    if ALLOW_SCRAPPING:
+        bool_ = extract_contents_for(urls)
+        print(bool_)
+        
 
 if __name__ == "__main__":
     main()
